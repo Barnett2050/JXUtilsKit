2722d401fc49203ec7b9ec1d245e36d458588897
Barnett <493650065@qq.com>
2020-03-25 16:41:19 +0800

    first commit

---
+++ JXUtilsKit/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  JXUtilsKit
+//
+//  Created by Barnett on 2020/3/25.
+//  Copyright © 2020 Barnett. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
